06c2f8a808a80b23b27f48885e4eb38dd1b7c333
Necmttn <necmettin.karakaya@gmail.com>
2017-08-06 19:05:26 +0800

    [jsx] update rules

--- twine.js
+++ twine.js
@@ -1,5 +1,4 @@
-import * as babel from 'babel-core';
-
+import * as babel from 'babel-core'
 
 export default function twine() {
   return {
@@ -9,7 +8,15 @@ export default function twine() {
         if (path.node.value === 'Hello') {
           path.node.value = 'World';
         }
-      }
+      },
+      JSXIdentifier(path) {
+        console.log(path.node)
+        if (path.node.name === "tw-storydata") {
+          path.node.name = "coolComponent";
+        }
+        if (path.node.name === "style") {
+        }
+      },
     }
   };
 }
