d6840ecb17e267be22f771e91d662df6cbdaf58d
Necmttn <necmettin.karakaya@gmail.com>
2017-08-06 17:25:16 +0800

    add babel to index file

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
-// require('babel-core/register');
-// require('babel-polyfill');
+require('babel-core/register');
+require('babel-polyfill');
 // require('./__test__/__fixtures__/first-test/code.js')
-require('twine')
+require('./twine.js')
